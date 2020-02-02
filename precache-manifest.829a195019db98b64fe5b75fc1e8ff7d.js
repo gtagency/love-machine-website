07781fc337e6833d8aa2572ee25eddfe41a53d81
Kevin Park <kevin.park1217@gmail.com>
2020-02-02 00:18:48 -0500

    Updates

---
+++ precache-manifest.829a195019db98b64fe5b75fc1e8ff7d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21804d28811db468c1d3e1dafefb8ec5",
+    "url": "/love-machine/index.html"
+  },
+  {
+    "revision": "7bb1ce0ca8f916b67749",
+    "url": "/love-machine/static/css/main.b48356af.chunk.css"
+  },
+  {
+    "revision": "a88cbac2a7b56bb8e7ee",
+    "url": "/love-machine/static/js/2.2a63cb8f.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/love-machine/static/js/2.2a63cb8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bb1ce0ca8f916b67749",
+    "url": "/love-machine/static/js/main.65d27d30.chunk.js"
+  },
+  {
+    "revision": "9f5294454379c4ac70c6",
+    "url": "/love-machine/static/js/runtime-main.921cb476.js"
+  },
+  {
+    "revision": "b398d28686c9857111c7b5d5c620e5a4",
+    "url": "/love-machine/static/media/agency-black.b398d286.png"
+  },
+  {
+    "revision": "03a4d089e8a30c71c1e133a8a40c51be",
+    "url": "/love-machine/static/media/agency-white.03a4d089.png"
+  },
+  {
+    "revision": "15024af34a9b13259a8beaf13f8afdac",
+    "url": "/love-machine/static/media/coc-transparent.15024af3.png"
+  }
+]);
